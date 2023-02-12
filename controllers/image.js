b9760b21b72986a25e9298fc7eafeae411298e49
TexterD <TexterD@github.com>
2023-02-12 23:04:19 +0100

    adding project

---
+++ controllers/image.js
@@ -0,0 +1,71 @@
+const { ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 48a473ce7cd542a491b537d28abb3ddd");
+
+
+// const Clarifai = require('clarifai');
+// console.log(Clarifai);
+
+// const app = new Clarifai.App({
+//  apiKey: '48a473ce7cd542a491b537d28abb3ddd'
+// });
+
+const handleApiCall = (req, res) => {
+	stub.PostModelOutputs(
+	  {
+	    // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+	    model_id: "a403429f2ddf4b49b307e318f00e528b",
+	    inputs: [{data: {image: {url: req.body.input}}}]
+	  },
+	  metadata,
+	  (err, response) => {
+	    if (err) {
+	      console.log("Error: " + err);
+	      return;
+	    }
+
+	    if (response.status.code !== 10000) {
+	      console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+	      return;
+	    }
+
+	    console.log("Predicted concepts, with confidence values:")
+	    for (const c of response.outputs[0].data.concepts) {
+	      console.log(c.name + ": " + c.value);
+	    }
+	    res.json(response);
+	  }
+	);
+
+	// app.models.predict(
+	//   {
+	//     id: 'face-detection',
+	//     name: 'face-detection',
+	//     version: '6dc7e46bc9124c5c8824be4822abe105',
+	//     type: 'visual-detector',
+	//   }, req.body.input
+	// )
+	// .then(data => {
+	// 	res.json(data);
+	// })
+	// .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0].entries);
+		})
+		.catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+};
